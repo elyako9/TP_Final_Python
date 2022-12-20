6f464e1b7aa48b6d2228d5aac65873d98a658767
elyako9 <edyakoby@gmail.com>
2022-12-20 18:47:57 -0300

    request para revisar (no funciona el ingreso de datos manual)

--- getdata.py
+++ getdata.py
@@ -3,9 +3,7 @@ import requests
 api_key = "6_EaRTnvrjkOlmezcLQ2P6m7ldhHrzMg"
 
 ticker = input("ingrese el nombre de la Acción:")
-
 fechainicio = input("ingrese la fecha de inicio:")
-
 fechafin = input("ingrese la fecha de fin:")
 
 api_url = f'https://api.polygon.io/v2/aggs/ticker/{ticker}/range/1/day/{fechainicio}/{fechafin}?adjusted=true&sort=asc&limit=120&apiKey={api_key}'

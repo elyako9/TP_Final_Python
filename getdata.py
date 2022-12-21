98a72fc4256c071bbd59801d2c1950ac96924125
elyako9 <edyakoby@gmail.com>
2022-12-21 17:55:13 -0300

    a

--- getdata.py
+++ getdata.py
@@ -1,13 +1,11 @@
 import requests
 
-api_key = "6_EaRTnvrjkOlmezcLQ2P6m7ldhHrzMg"
-
-ticker = input("ingrese el nombre de la Acción:")
-fechainicio = input("ingrese la fecha de inicio:")
-fechafin = input("ingrese la fecha de fin:")
-
-api_url = f'https://api.polygon.io/v2/aggs/ticker/{ticker}/range/1/day/{fechainicio}/{fechafin}?adjusted=true&sort=asc&limit=120&apiKey={api_key}'
-
-data = requests.get(api_url).json()
-
-print(api_url)
+def pedirdatos():
+    api_key = "6_EaRTnvrjkOlmezcLQ2P6m7ldhHrzMg"
+    ticker = input("ingrese el nombre de la Acción:")
+    fechainicio = input("ingrese la fecha de inicio:")
+    fechafin = input("ingrese la fecha de fin:")
+    api_url = f'https://api.polygon.io/v2/aggs/ticker/{ticker}/range/1/day/{fechainicio}/{fechafin}?adjusted=true&sort=asc&limit=120&apiKey={api_key}'
+    r = requests.get(api_url)
+    data = r.json()
+    return data
--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-while True:
+from getdata import pedirdatos
+
+while True: ##while para el programa se ejecute 
   op = int(input("Elija una acción:\n1.Actualización de datos\n2.Visualización de datos\nPara salir presione cualquier otro número\n"))
   if op == 1:
-    ticker = input("ingrese el nombre de la Acción:")
-    fecha_ini = input("ingrese la fecha de inicio:")
-    fecha_cierre = input("ingrese la fecha de fin:")
-    print(f"Accion {ticker} desde {fecha_ini} hasta {fecha_cierre}")
+    # ticker = str(input("ingrese el nombre de la Acción:"))
+    # fecha_ini = str(input("ingrese la fecha de inicio:"))
+    # fecha_cierre = str(input("ingrese la fecha de fin:"))
+    # print(f"Accion {ticker} desde {fecha_ini} hasta {fecha_cierre}")
+    data = pedirdatos()
+    print(data)
   elif op == 2:
     visu = int(input("Elija tipo de visualización:\n1.Resumen\n2.Grafico\n3.Para volver al menu anterior\n"))
     #print("opcion 2") #cuando sale un de estas que dispare una funcion o objero no se para hacer la grafica

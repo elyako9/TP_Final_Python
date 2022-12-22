5c10642d513d892e6d2bfc4c959c498860a76830
elyako9 <edyakoby@gmail.com>
2022-12-21 21:01:08 -0300

    menu principal

--- main.py
+++ main.py
@@ -1,20 +1,22 @@
-from getdata import pedirdatos
+from getdata import actualizaracciones
+from resudata import resumen
 
 while True: ##while para el programa se ejecute 
   op = int(input("Elija una acción:\n1.Actualización de datos\n2.Visualización de datos\nPara salir presione cualquier otro número\n"))
   if op == 1:
-    # ticker = str(input("ingrese el nombre de la Acción:"))
-    # fecha_ini = str(input("ingrese la fecha de inicio:"))
-    # fecha_cierre = str(input("ingrese la fecha de fin:"))
-    # print(f"Accion {ticker} desde {fecha_ini} hasta {fecha_cierre}")
-    data = pedirdatos()
-    print(data)
+    actualizaracciones()
+    #copiaradb(data)
+    print('Los datos se actualizaron exitosamente')
+
   elif op == 2:
     visu = int(input("Elija tipo de visualización:\n1.Resumen\n2.Grafico\n3.Para volver al menu anterior\n"))
-    #print("opcion 2") #cuando sale un de estas que dispare una funcion o objero no se para hacer la grafica
+    
     if visu == 1:
-      print("disparar funcion 1")
+      print('Datos almacenados en Base de Datos:')
+      resumen()
+
     elif visu == 2:
       print("disparar funcion 2")      
+  
   else:
     break
